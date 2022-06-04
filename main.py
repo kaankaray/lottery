645360ceab90e692dbbfd0e0d496aa5e72f3821a
kaankaray <kaankaray99@gmail.com>
2022-06-05 00:59:50 +0300

    project reborn.
    Front-end is pretty much almost finished.
    
    - Need to add the address on top, mb. clock?

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request, make_response
+from threading import Thread
+import worker as w
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
-print('')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return 'Hello, World!'
+    # In case I need button handling.
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Do Something':
+            pass  # do something
+        elif request.form['submit_button'] == 'Do Something Else':
+            pass  # do something else
+        else:
+            pass  # unknown
+    elif request.method == 'GET':
+        print(len(w.transactions))
+        return render_template('main.html', value=w.transactions)
+
+
+def test():
+    print('test')
+
 
 if __name__ == '__main__':
-    print('PyCharm')
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(test, 'cron', second=3)
+    scheduler.start()
+    t = Thread(target=w.execute, args=())
+    t.start()
+    # pool = Pool(processes=1) # Am I gonna need multiple threads?
+    # pool.apply_async(w.execute)
     app.run()
-
+    scheduler.shutdown()
