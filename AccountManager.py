e18436b4e9b47af43dbddb4fd04d52947d8ac065
kaankaray <kaankaray99@gmail.com>
2022-06-09 23:17:57 +0300

    Made some internal methods private, added 'privKey' param to __init__

--- AccountManager.py
+++ AccountManager.py
@@ -7,7 +7,7 @@ import codecs
 
 class AccountManager(object):
     """
-    Ether account creator and conventer
+    Ether account creator and converter
     """
 
     _privateKey = '0'
@@ -20,7 +20,7 @@ class AccountManager(object):
     def fetchPublicKey(self):
         return self._publicKey
 
-    def privKeyToPublic(self, inputByte):
+    def _privKeyToPublic(self, inputByte):
         private_key_bytes = codecs.decode(inputByte, 'hex')
         key = SigningKey.from_string(private_key_bytes, curve=SECP256k1).verifying_key
         key_bytes = key.to_string()
@@ -28,17 +28,17 @@ class AccountManager(object):
         # logging.debug(f'{inputByte} -> {key_hex}')
         return key_hex
 
-    def publicKeyToWallet(self, inputByte):
+    def _publicKeyToWallet(self, inputByte):
         public_key_bytes = codecs.decode(inputByte, 'hex')
         keccak_digest = keccak.new(digest_bits=256).update(public_key_bytes).hexdigest()
         # logging.debug(f'{inputByte} -> {"0x" + keccak_digest[-40:]}')
         return '0x' + keccak_digest[-40:]
 
-    def __init__(self, randomSeed):
+    def __init__(self, privKey=None, randomSeed=10):
         random.seed(randomSeed)
         while len(self._privateKey) != 66:
-            self._privateKey = str(hex(random.getrandbits(256)))
-        self._publicKey = self.privKeyToPublic(self._privateKey[2:])
-        self.walletVersion = self.publicKeyToWallet(self._publicKey)
+            self._privateKey = privKey if privKey else str(hex(random.getrandbits(256)))
+        self._publicKey = self._privKeyToPublic(self._privateKey[2:])
+        self.walletVersion = self._publicKeyToWallet(self._publicKey)
 
         logging.info(f'Wallet created: {self.walletVersion}')
